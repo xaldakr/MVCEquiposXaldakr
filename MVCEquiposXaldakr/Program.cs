cb9f047a33175f25ffc9360376dd225312f757e7
xaldakr <xavieravilaposada@gmail.com>
2024-03-23 11:56:34 -0600

    fixes

--- MVCEquiposXaldakr/Program.cs
+++ MVCEquiposXaldakr/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Equipos}/{action=Index}/{id?}");
 
 app.Run();
